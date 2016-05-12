c62034700658407082cafebd7902e0133c9d2baf
Nathan Conley <nateconley123@gmail.com>
2016-05-12 00:43:03 -0700

    Sticky footer fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const maps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function(){
+	browserSync.init({
+		open: false,
+		proxy: 'localhost:8888'
+	});
+});
 
 gulp.task('sass', function(){
 	return gulp.src('sass/main.sass')
@@ -10,10 +18,11 @@ gulp.task('sass', function(){
 		.pipe(sass())
 		.pipe(maps.write('./'))
 		.pipe(gulp.dest('css/'))
+		.pipe(browserSync.stream())
 });
 
 gulp.task('watch', ['sass'], function(){
 	gulp.watch('sass/*.sass', ['sass']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['browser-sync', 'watch']);
