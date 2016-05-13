252ca9e2d7181e43d36bcdab3d64ad2e33c28ec7
Nathan Conley <nateconley123@gmail.com>
2016-05-12 19:03:11 -0700

    Pricing tables style

--- page-photography.php
+++ page-photography.php
@@ -4,7 +4,7 @@
 
 <div class="page-info">
 	<div class="inner-page-info">
-		<h1><?php the_title(); ?></h2>
+		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	</div>
 </div>
--- page-videography.php
+++ page-videography.php
@@ -5,12 +5,29 @@
 
 <div class="page-info">
 	<div class="inner-page-info">
-		<h1><?php the_title(); ?></h2>
+		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	</div>
 </div>
 
 <div class="container">
+
+
+	<div class="pricing-table">
+		<div class="pricing-header">
+			<h3 class="title">PHOTOGRAPHY</h3>
+			<h3 class="price">$125</h3>
+		</div>
+		<ul>
+			<li>One Camera</li>
+			<li>One Location</li>
+			<li>Two Hours</li>
+			<li>Ten Edits</li>
+			<li>Additional Locations - $30 per 100 miles</li>
+			<li>Additional Hours - $20 per hour</li>
+		</ul>
+	</div>
+
 
 </div>
 
