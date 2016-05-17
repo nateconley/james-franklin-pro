c64fdef3e5e1e79e113b93d332321fd4a248717d
Nathan Conley <nateconley123@gmail.com>
2016-05-16 21:58:32 -0700

    Footer style and addition of social links

--- footer.php
+++ footer.php
@@ -1,6 +1,21 @@
 	<footer>
-		<p>&copy; <?php echo date('Y'); ?> James Franklin Productions</p>
-		<p>Website by <a href="http://nateconley.com" target="_blank">Nate Conley</a></p>
+		<div class="inner-footer">
+			<div class="footer-text">
+				<p>&copy; <?php echo date('Y'); ?> James Franklin Productions</p>
+				<p>Website by <a href="http://nateconley.com" target="_blank">Nate Conley</a></p>
+			</div>
+			<div class="social-links">
+				<a href="">
+					<i class="fa fa-instagram"></i>
+				</a>
+				<a href="">
+					<i class="fa fa-twitter"></i>
+				</a>
+				<a href="">
+					<i class="fa fa-snapchat"></i>
+				</a>
+			</div>
+		</div>
 	</footer>
 	<?php wp_footer(); ?>
 </body>
